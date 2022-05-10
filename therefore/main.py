1bba00e8260f051f4057183d542db04ffb8c582c
Jackson Morgan <jacksonmorgan@pop-os.localdomain>
2022-05-09 18:45:09 -0700

    end of day

--- therefore/main.py
+++ therefore/main.py
@@ -10,7 +10,6 @@ date: May 9th 2022
 
 import numpy as np
 import matplotlib as pyplot
-import sympy as sym
 
 #utility functions
 def ScalarFlux(angular_flux, weights):
@@ -23,24 +22,37 @@ def HasItConverged(scalar_flux_next, scalar_flux):
    np.allclose(scalar_flux_next, scalar_flux, rtol=tol)
 
 
+SimpleCorn
 
 
 #simple corner balance
-def SimpleCornerBalance():
+def SimpleCornerBalanceRight(Q_l, Q_r, psi_mh, xsec, dx, mu):
+    
+    laguz  = (xsec * dx) / 4
+    mannaz = mu/2 - laguz
+    othala = laguz + mu/2
+    
+    denominator = mu/2 + (mannaz*mu)/(2*othala) + laguz*(mannaz/othala + 1)
+    
+    psi_l = (dx/2*Q_l - ((mu*dx)/(4*othala) + (xsec*dx**2)/(6*othala))*Q_r + mu*psi_mh) / denominator
+    
+    psi_r = psi_l*mannaz/othala + dx/(2*othala)*Q_r
+    
+    return(psi_l, psi_r)
+    
+def SimpleCornerBalanceLeft():
     Q_l = 
     Q_r = 
     
     laguz  = (xsec * dx) / 4
     mannaz = mu/2 - laguz
-    othala = laguz - mu/2
+    othala = laguz + mu/2
     
-    denominator = mu/2 + mannaz/(2*othala) + laguz*mannaz/othala + laguz
+    denominator = mu/2 + (mannaz*mu)/(2*othala) + laguz*(mannaz/othala + 1)
     
-    teiwaz = dx/2 * (Q_r - mu * psi_m_ph)/othala
+    psi_l = (dx/2*Q_l - ((mu*dx)/(4*othala) + (xsec*dx**2)/(6*othala))*Q_r + mu*psi_mh) / denominator
     
-    psi_ml = (dx/2*Q_l - mu/2*teiwaz + mu*psi_m_mh - laguz*teiwaz) / denominator
-    
-    psi_mr = psi_ml*mannaz/othala + teiwaz
+    psi_r = psi_ml*mannaz/othala + dx/(2*othala)*Q_r
     
     return(psi_ml, psi_mr)
 
@@ -86,14 +98,14 @@ while source_convergence == False:
     
     # TODO: Discrete Ordinants: Sweep Left to Right (+)
     for mu in range(N_angles):
-        for i in range(N_cells)
-            angular_flux_next[mu, i] = simple_corner_balance()
+        for i in range(N_cells):
+            angular_flux_next[mu, i] = SimpleCornerBalanceRight(Q_l, Q_r, psi_mh, xsec, dx, mu)
             
     
     # TODO: Discrete Ordinants: Sweep Right to Left (-)  
     for mu in range(N_angles)
-        for i in range(N_cells)
-            angular_flux_next[mu, i] = simple_corner_balance()
+        for i in range(N_cells):
+            angular_flux_next[mu, i] = SimpleCornerBalance()
     
     scalar_flux_next = ScalarFlux(angular_flux_next, weights_gq)
     
